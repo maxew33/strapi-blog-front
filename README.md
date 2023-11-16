d66f0e73cdb548594eac4b310d116b41dff576df
Maxime Malfilâtre <maxime.malfilatre@gmail.com>
2023-11-16 15:41:02 +0100

    :tada: Journey begins

---
+++ README.md
@@ -0,0 +1,3 @@
+# strapi blog - partie front
+
+Projet pour apprendre à utiliser strapi
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <>
+            <h1>My great strapi blog</h1>
+            <p>let learn this awesome headless CMS</p>
+        </>
+    )
+}
+
+export default App
